ac11708566cebffe87f6a512d3d95dabb5f1d2bc
Yana Slavcheva <yana.slavcheva@gmail.com>
2014-08-27 01:00:59 +0300

    Added aside include

---
+++ includes/aside.php
@@ -0,0 +1,48 @@
+<aside>
+    <div class="categories">
+        <nav>
+            <ul>
+                <?php
+                $categories = getCategories();
+
+                foreach ($categories as $category) {
+                    echo '<li>';
+
+                    echo '<a href="' . 'index.php?catid=' . $category['category_ID'] . '">' . $category['category_name'] . '</a>';
+
+                    echo '</li>';
+                }
+
+                ?>
+            </ul>
+        </nav>
+    </div>
+    <div class="socials">
+        <ul>
+            <li>
+                <a href="">FAQ</a>
+            </li>
+            <li>
+                <a href="">Contacts</a>
+            </li>
+            <li>
+                <a href="">Help</a>
+            </li>
+            <li>
+                <img src="pictures/giHubImg.png" />
+            </li>
+            <li>
+                <div id="fb-root" ></div>
+                <script>
+                    (function (d, s, id) {
+                        var js, fjs = d.getElementsByTagName(s)[0];
+                        if (d.getElementById(id)) return;
+                        js = d.createElement(s); js.id = id;
+                        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));</script>
+                <div style="background-color:white" class="fb-like-box" data-href="https://www.facebook.com/pages/Saksiacom/1605229196370501?sk=info" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="false"></div>
+            </li>
+        </ul>
+    </div>
+</aside>
